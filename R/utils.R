35a4966c8825a5e6131dd653b98beb461ebf74a3
matthewcurrier <8642088+matthewcurrier@users.noreply.github.com>
2026-05-14 12:26:07 -0700

    removed dead files that were supporting editable table ui and server, which has been deleted

--- R/utils.R
+++
@@ -1,5 +0,0 @@
-# R/utils.R
-#
-# Grade-level utilities have moved to R/grade-utils.R.
-# This file is intentionally empty. It will be removed in a future
-# release once all callers reference grade-utils.R directly.
